059da6fc370105c83667428efcbfc1bf6daada01
Paint1997 <82351282+Paint1997@users.noreply.github.com>
2021-04-12 21:30:07 -0700

    Update hpain001_lab2_part1.c

--- hpain001_lab2_part1.c
+++ hpain001_lab2_part1.c
@@ -1,13 +1,4 @@
-    /* Insert DDR and PORT initializations */
-        DDRA = 0x00; PORTA = 0xFF;
-        DDRrtner(s) Name:
- *      Lab Section:
- *       *      Assignment: Lab #  Exercise #
- *        *      Exercise Description: [optional - include for your own benefit]
- *         *
- *          *      I acknowledge all content contained herein, excluding template or example
- *           *      code, is my own original work.
- *            */
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
